c5c32ef59b06606f0083510aca769ac15a54c262
pumvandekoppel <pumvandekoppel@gmail.com>
2024-03-08 17:06:35 +0100

    more of the same and margin-top about text

--- hide.js
+++ hide.js
@@ -2,14 +2,14 @@ var hidden = new Boolean(false);
 
 function hide() {
     if (hidden == false) {
-        document.getElementById("hidethisheader").style.display = "none";
-        document.getElementById("hidethisfooter").style.display = "none";
+        document.getElementById("hidethisheader").style.visibility = "hidden";
+        document.getElementById("hidethisfooter").style.visibility = "hidden";
         hidden = true
         document.getElementById("focus").style.cursor = "zoom-out"
     }
     else {
-        document.getElementById("hidethisheader").style.display = "flex";
-        document.getElementById("hidethisfooter").style.display = "flex";
+        document.getElementById("hidethisheader").style.visibility = "visible";
+        document.getElementById("hidethisfooter").style.visibility = "visible";
         hidden = false
         document.getElementById("focus").style.cursor = "zoom-in"
     }
